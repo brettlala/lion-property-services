330c57a4a217f3bd6d598f053a483ac2c786eb70
brett <lyonsbrett17@gmail.com>
2023-03-21 17:22:27 -0400

    added the hamburger menu on mobile

--- js/script.js
+++ js/script.js
@@ -1,52 +1,36 @@
 document.addEventListener('headerAndFooterLoaded', function () {
   (function () {
-    const hamburger = document.querySelector('.hamburger');
-    const navLinks = document.querySelector('.nav-links');
-    const links = document.querySelectorAll('.nav-links li');
-    const overlay = document.querySelector('.overlay');
     const ctaButton = document.querySelector('.cta-button');
     const formModal = document.getElementById('form-modal');
     const closeModal = document.querySelector('.close');
     const form = document.querySelector('.quote-form-pop-up');
     const mainContent = document.querySelector('main');
     const thankYouModal = document.getElementById('thank-you-modal');
-    
-    let overlayActive = false;
-    
-    function toggleMenu() {
-      hamburger.classList.toggle('active');
-      navLinks.classList.toggle('nav-active');
-      overlay.classList.toggle('active');
-      overlayActive = !overlayActive;
-      
-      if (overlayActive) {
-        document.addEventListener('click', outsideClickListener);
-      } else {
-        document.removeEventListener('click', outsideClickListener);
-      }
-      
-      links.forEach((link, index) => {
-        if (link.style.animation) {
-          link.style.animation = '';
-        } else {
-          link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-        }
-      });
-    }
 
-    function outsideClickListener(event) {
-      if (!navLinks.contains(event.target) && event.target !== hamburger) {
-        toggleMenu();
-      }
-    }
+    // Hamburger 1 start
 
-    hamburger.addEventListener('click', toggleMenu);
 
-    overlay.addEventListener('click', (event) => {
-      if (!navLinks.contains(event.target)) {
-        toggleMenu();
-      }
-    });
+
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const mobileNav = document.querySelector('.nav-mobile');
+
+hamburgerMenu.addEventListener('click', () => {
+  // Toggle the mobile navigation and overlay
+  mobileNav.classList.toggle('active');
+  document.body.classList.toggle('overlay-active');
+});
+
+mobileNav.addEventListener('click', (event) => {
+  // Close the mobile navigation when a link is clicked
+  if (event.target.tagName === 'A') {
+    mobileNav.classList.remove('active');
+    document.body.classList.remove('overlay-active');
+  }
+});
+
+
+
+    // Hamburger 1 end
 
     if (ctaButton) {
       ctaButton.addEventListener('click', (e) => {
@@ -82,24 +66,11 @@ document.addEventListener('headerAndFooterLoaded', function () {
       }
     });
 
-    hamburger.addEventListener('click', () => {
-      mainContent.classList.toggle('blurry-background');
-      navLinks.classList.toggle('bigger-font');
-      overlayActive = !overlayActive;
-    });
-
-    window.addEventListener('resize', () => {
-      if (window.innerWidth > 700 && !overlayActive) {
-        mainContent.classList.remove('blurry-background');
-        navLinks.classList.remove('bigger-font');
-      }
-    });
-
-    function showThankYouModal(formToSubmit) {
-  thankYouModal.style.display = 'block';
-  const closeModal = thankYouModal.querySelector('.close');
-  closeModal.onclick = () => {
-    thankYouModal.style.display = 'none';
+  function showThankYouModal(formToSubmit) {
+    thankYouModal.style.display = 'block';
+    const closeModal = thankYouModal.querySelector('.close');
+    closeModal.onclick = () => {
+      thankYouModal.style.display = 'none';
   };
   // Submit the form after showing the thank you modal
   formToSubmit.submit();
