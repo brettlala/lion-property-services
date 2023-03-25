6670164f055d06a6edd1dc9286db0f5385a3820d
brett <lyonsbrett17@gmail.com>
2023-03-24 23:34:16 -0400

    took off the pop up modal and all related code

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
 document.addEventListener('headerAndFooterLoaded', function () {
   (function () {
-    const ctaButton = document.querySelector('.cta-button');
-    const formModal = document.getElementById('form-modal');
-    const closeModal = document.querySelector('.close');
-    const form = document.querySelector('.quote-form-pop-up');
     const mainContent = document.querySelector('main');
 
 // Hamburger Menu start
@@ -24,48 +20,5 @@ mobileNav.addEventListener('click', (event) => {
   }
 });
 // Hamburger END
-
-    if (ctaButton) {
-      ctaButton.addEventListener('click', (e) => {
-        e.preventDefault();
-        formModal.style.display = 'block';
-      });
-    }
-
-    if (closeModal) {
-      closeModal.addEventListener('click', () => {
-        formModal.style.display = 'none';
-      });
-    }
-
-    window.addEventListener('click', (event) => {
-      if (event.target === formModal) {
-        formModal.style.display = 'none';
-      }
-    });
-
-  if (form) {
-      form.addEventListener('submit', (e) => {
-        e.preventDefault(); // prevent default form submission behavior
-
-        // Submit the form using AJAX
-        const xhr = new XMLHttpRequest();
-        xhr.open(form.method, form.action);
-        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        xhr.onreadystatechange = () => {
-          if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-              // Redirect to the thank you page after the form is submitted
-              window.location.href = 'thank-you.html';
-            } else {
-              console.error('Form submission failed: ' + xhr.status);
-            }
-          }
-        };
-        xhr.send(new FormData(form));
-
-        formModal.style.display = 'none';
-      });
-    }
   })();
 });
